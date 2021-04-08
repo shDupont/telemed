8594cdc4a1c8211437e8f3d35334ec727a47c8c2
Letícia Dupont <leticia.dupont20@gmail.com>
2021-04-08 19:03:42 -0300

    Initial commit

---
+++ med_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home),
+    path('signup/', views.signup),
+    path('login/', views.login),
+    path('aboutus/', views.aboutus),
+]
